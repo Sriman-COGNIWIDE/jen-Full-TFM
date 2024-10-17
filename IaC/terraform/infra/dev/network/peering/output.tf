9c0bd8296605b65769d439e333757db25f18f2ac
Sriman-COGNIWIDE <sriman.mayandi@cogniwide.com>
2024-10-17 17:52:02 +0530

    Delete IaC/terraform directory

--- IaC/terraform/infra/dev/network/peering/output.tf
+++
@@ -1,3 +0,0 @@
-output "vpc_peer_id" {
-  value = module.vpc_peering.peer_id
-}
--- IaC/terraform/modules/network/subnet/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_subnet" "subnet" {
-  vpc_id            = var.vpc_id
-  cidr_block        = var.subnet_cidr
-  availability_zone = var.subnet_az
-
-  tags = {
-    Name = var.subnet_name
-  }
-}
