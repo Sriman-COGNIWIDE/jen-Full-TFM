25aebf46939b533b70564662a95567506763cb76
Sriman-COGNIWIDE <sriman.mayandi@cogniwide.com>
2024-10-20 23:55:23 +0530

    Delete IaC/terraform directory

--- IaC/terraform/infra/dev/compute/ec2/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "home-bucks-tfs"
-    key            = "nets-ec2/nets-ec2.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "state-lock"
-  }
-}
