14ffba9aeb608bd7be12c9e2515c482079d892d9
Sriman-COGNIWIDE <sriman.mayandi@cogniwide.com>
2024-10-21 10:02:05 +0530

    Delete IaC/terraform directory

--- IaC/terraform/infra/dev/database/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "tf-prj-recipe"
-    key            = "database/database.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "tf-prj-recipe"
-  }
-}
