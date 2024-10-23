03c3212ffbaad8f65c9f294ac456f10a41156f71
Sriman-COGNIWIDE <sriman.mayandi@cogniwide.com>
2024-10-23 09:45:53 +0530

    Delete IaC/terraform directory

--- IaC/terraform/infra/dev/serverless/lambda/locals.tf
+++
@@ -1,3 +0,0 @@
-locals {
-  lambda_creds = jsondecode(data.aws_secretsmanager_secret_version.lambda_credentials.secret_string)
-}
--- IaC/terraform/modules/network/vpc/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "cidr_block" {
-  type        = string
-  description = "VPC cidr block"
-}
-
-variable "vpc_name" {
-  type        = string
-  description = "VPC name"
-}
