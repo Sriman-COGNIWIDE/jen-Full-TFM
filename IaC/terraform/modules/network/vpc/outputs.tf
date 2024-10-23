2fcbb27b27da893addbfb3627c1d0c7f8d9fae08
Sriman-COGNIWIDE <sriman.mayandi@cogniwide.com>
2024-10-23 15:34:41 +0530

    Delete IaC/terraform directory

--- IaC/terraform/modules/network/vpc/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vpc_id" {
-  value       = aws_vpc.vpcs.id
-  description = "The ID of the VPC"
-}
-
-output "vpc_cidr_block" {
-  value       = aws_vpc.vpcs.cidr_block
-  description = "Cidr for the vpc"
-}
