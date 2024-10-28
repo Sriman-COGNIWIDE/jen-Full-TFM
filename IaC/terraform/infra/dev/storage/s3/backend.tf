a920929461e53e13108323cd0b8657da64226526
Sriman-COGNIWIDE <sriman.mayandi@cogniwide.com>
2024-10-28 14:59:16 +0530

    Delete IaC/terraform directory

--- IaC/terraform/infra/dev/storage/s3/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "tf-prj-recipe"
-    key            = "recipe-storage/s3.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "tf-prj-recipe"
-  }
-}
--- IaC/terraform/modules/network/subnet/output.tf
+++
@@ -1,9 +0,0 @@
-output "subnet_id" {
-  value       = aws_subnet.subnet.id
-  description = "Subnet ID"
-}
-
-output "subnet_cidr" {
-  value       = aws_subnet.subnet.cidr_block
-  description = "subet's cidr"
-}
